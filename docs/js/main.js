c632e38ac33138ed9979ff61aca48c84860de3ae
sharon_monroy <mancotatiana21@gmail.com>
2023-01-20 16:05:42 -0500

    se agrega  cambio

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,16 +1,18 @@
-'use ';
+"use ";
 
-window.onscroll = function() { efectoHabilidades() };
+window.onscroll = function () {
+  efectoHabilidades();
+};
 
 //funcion que aplica la animación de la barra de habilidades
 function efectoHabilidades() {
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if (distancia_skills >= 300) {
-        document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("bd").classList.add("barra-progreso3");
-        document.getElementById("ps").classList.add("barra-progreso4");
-    }
-
+  var skills = document.getElementById("skills");
+  var distancia_skills =
+    window.innerHeight - skills.getBoundingClientRect().top;
+  if (distancia_skills >= 300) {
+    document.getElementById("html").classList.add("barra-progreso1");
+    document.getElementById("cs").classList.add("barra-progreso2");
+    document.getElementById("js").classList.add("barra-progreso3");
+    document.getElementById("rc").classList.add("barra-progreso4");
+  }
 }
